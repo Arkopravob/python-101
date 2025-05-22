6a1459b46ba3b4a76e42e3e60fcceaadcebb3d16
Arkopravob <arka.bhuin@gmail.com>
2025-05-22 10:41:43 +0530

    list

--- List/list.py
+++ List/list.py
@@ -1,2 +1,16 @@
 a = ["abc" ,"cde","efg"]
 print(a)
+numbers = [10, 20, 30, 40]
+names = ["Alice", "Bob", "Charlie"]
+mixed = [1, "hi", 3.14, True]
+print(names[1])
+print(mixed[3])
+#common operations
+#access item
+print(numbers[2])
+#change item
+names[2]="will-jacks"
+print(names)
+#add item in last in the list
+names.append("ram")
+print(names)
