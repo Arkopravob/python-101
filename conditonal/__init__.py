8e0f6d6b9277b4cfc036106f208bb83fc337338a
Arkopravob <arka.bhuin@gmail.com>
2025-08-02 18:14:20 +0530

    voter_eligibilty

--- conditonal/__init__.py
+++ conditonal/__init__.py
@@ -4,3 +4,11 @@ if num > 10:
     print("yes")
 else:
     print("No")
+
+"""vaild voter check"""
+
+voter_age = int(input("enter the age: "))
+if voter_age >= 18:
+    print("yes you are eligble for vote")
+else:
+    print("go and play with teeneger")
