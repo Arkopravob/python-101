626c127ba22098fab53b3e8dd42b6e330bf02bb8
Arkopravob <arka.bhuin@gmail.com>
2025-08-03 14:25:20 +0530

    prime check

---
+++ loop/primeNumberCheckwithRange.py
@@ -0,0 +1,9 @@
+n = int(input("tell the number: "))
+count = 0
+for i in range(1,n+1):
+    if n%i ==0:
+        count = count + 1
+if count== 2:
+     print("its a prime number ")
+else:
+    print("its not a prime number")
