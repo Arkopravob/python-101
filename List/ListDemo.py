b59d5088505f560456ca1e8c5b9f64ec0e5745f1
Arkopravob <arka.bhuin@gmail.com>
2025-08-17 21:49:06 +0530

    list code added

---
+++ List/ListDemo.py
@@ -0,0 +1,13 @@
+a = [12,3,5,6,8,10,12]
+"""indexing of list"""
+print(a[3])
+"""slicing"""
+print(a[0:3])
+
+"""traverasing"""
+for i in range(len(a)):
+    print(a[i])
+
+#2nd way direct value
+for i in a:
+    print(i)
