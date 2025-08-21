29f65e163ae8e4ee5286c900ecf975f91ba54ff0
Arkopravob <arka.bhuin@gmail.com>
2025-08-21 16:08:52 +0530

    function new code added

---
+++ function/function02.py
@@ -0,0 +1,22 @@
+def useless(num1,num2,num3):
+    return num3
+
+print(useless(1,5,8))
+
+def greet(str):
+    print("hey: ",str)
+
+greet("arko")
+
+def sayHello():
+    """print hello"""
+    print("hi! python")
+sayHello()
+
+def double(value):
+    """print double of any number"""
+    return value *2
+
+print(double(4))
+print(double(12))
+
