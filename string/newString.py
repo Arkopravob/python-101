026bcd24e5fd019d06dedb0a0aeeb96d5197336c
Arkopravob <arka.bhuin@gmail.com>
2025-09-01 08:15:15 +0530

    string ops update

---
+++ string/newString.py
@@ -0,0 +1,32 @@
+str ='kolkata'
+str1 ="bombay"
+str2 = """hyper visor"""
+print(str)
+# slice is a segement of a string
+#slicing
+
+
+a = str1[1:4]
+print(a)
+
+#string indexing
+print(str[2])
+print(str[4])
+print(str[-1])
+
+#update
+s = str[2] +"ab!"
+print(s)
+
+print(str[0])
+print(str[0:1])
+s1 = (str[0] == str[0:1])
+print(s1)
+
+#for list
+
+factor =[1,4,201,"flask","dog"]
+print(factor[0])
+print(factor[0:1])
+res = (factor[0] == factor[0:1])
+print(res)
